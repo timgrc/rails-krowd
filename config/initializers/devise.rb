59f94b4805dc32ac745aa0700535e8fc56457fe5
Tom Jeanniot <tom.jeanniot@live.fr>
2016-12-06 12:47:37 +0100

    installer gems

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,4 +271,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+    scope: 'address'
+
 end

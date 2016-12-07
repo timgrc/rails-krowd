16a0ddccd93fda9f68b1962fd69adef7b359f9d9
Timothee Garcia <grc.tmt@gmail.com>
2016-12-07 01:48:13 +0100

    add group with modal

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,7 @@ class GroupsController < ApplicationController
   before_action :find_group, only: [:show] # :destoy ?
 
   def index
+    @groups     = Group.all
     @group      = Group.new
     @yam_groups = GetAllGroups.new(current_user.access_token).list
   end
@@ -18,11 +19,16 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = current_user.groups.build(group_params)
+    yam_groups            = GetAllGroups.new(current_user.access_token).list
+    group_params_from_api = yam_groups.find do |yam_group|
+      yam_group[:full_name] == params[:group][:full_name]
+    end
+    @group = current_user.groups.build(group_params_from_api)
     # authorize @group
 
     if @group.save
-      redirect_to group_path(@group)
+      # redirect_to group_path(@group)
+      redirect_to groups_path
     else
       render 'new'
     end

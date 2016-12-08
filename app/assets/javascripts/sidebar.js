b74162a0595b264b967e50d021d7321e36d0f229
Pierre Vanoverschelde <pierre.vano@gmail.com>
2016-12-08 13:42:13 +0100

    editing sidebar

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -8,22 +8,4 @@ $(document).ready(function(){
 
 });
 
-$(document).ready(function(){
-  $('a.tab').bind('click', function(event) {
-    var anchor = $(this).attr('href');
 
-    $('html, body').stop().animate({
-        scrollTop: $(anchor).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-  });
-});
-
-// //jQuery to collapse the navbar on scroll
-// $(window).scroll(function() {
-//     if ($(".navbar-wagon").offset().top > 100) {
-//         $(".navbar-wagon").addClass("top-nav-collapse");
-//     } else {
-//         $(".navbar-wagon").removeClass("top-nav-collapse");
-//     }
-// });

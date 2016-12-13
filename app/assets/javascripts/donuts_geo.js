66d0b4a32d9cc8c5fd10bdb85765703a3b15aa85
Tom Jeanniot <tom.jeanniot@live.fr>
2016-12-13 16:38:17 +0100

    add of the 3 donuts

--- app/assets/javascripts/donuts_geo.js
+++ app/assets/javascripts/donuts_geo.js
@@ -2,10 +2,16 @@ $(function() {
   geoChartMap();
   donut();
   centerDonutInner();
+  first_donut();
+  second_donut();
+  third_donut();
 });
 
 $( window ).resize(function() {
   geoChartMap();
   donut();
   centerDonutInner();
+  first_donut();
+  second_donut();
+  third_donut();
 });

c833fa3e6a0d9f78f2792d97230236f18444e39e
Sarah Jammernegg <sjammernegg@gmail.com>
2016-12-14 17:20:19 +0100

    Remove group from home page

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,6 +2,16 @@ class MembershipsController < ApplicationController
   def destroy
     @membership = Membership.find(params[:id])
     authorize @membership
-    @membership.destroy
+    if @membership.destroy
+     respond_to do |format|
+        format.html { redirect_to membership_path(@membership) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html {redirect_to groups}
+        format.js
+      end
+    end
   end
 end

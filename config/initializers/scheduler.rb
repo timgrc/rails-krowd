afc85d57b830eababe344065f729389ea75312b0
Timothee Garcia <grc.tmt@gmail.com>
2016-12-15 14:18:08 +0100

    bot done

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,7 +2,7 @@ require 'sidekiq/scheduler'
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(Rails.root.join('config/sidekiq_scheduler.yml'))
+    Sidekiq.schedule = YAML.load_file(Rails.root.join('config/sidekiq_schedulers.yml'))
     Sidekiq::Scheduler.reload_schedule!
   end
 end

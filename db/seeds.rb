d8edefd2a8f7b79838542d3acd52c968b050f51f
Timothee Garcia <grc.tmt@gmail.com>
2017-01-02 16:16:03 +0100

    ruby version changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-Membership.delete_all
+# Membership.delete_all
 
-User.all.each do |user|
-  mem = Membership.new
-  mem.user = user
-  mem.group = Group.find_by_full_name('Innovation Challenge - The Company')
-  mem.save
-end
+# User.all.each do |user|
+#   mem = Membership.new
+#   mem.user = user
+#   mem.group = Group.find_by_full_name('Innovation Challenge - The Company')
+#   mem.save
+# end
 
 it1 = IncentiveTemplate.new
 it1.body = "Thank you guys for this awesome week! Keep going!"
